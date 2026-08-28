187d180e8632e8b3f18ff6eb73df229dc9675dcc
AlejandroV-alpha <va0035314@est.univalle.edu>
2026-08-28 11:27:26 -0400

    PRx v1.0.1
    Update production API base URL

--- prx-frontend/src/environments/environment.production.ts
+++ prx-frontend/src/environments/environment.production.ts
@@ -3,7 +3,7 @@ import { Environment } from '@env/environment.model';
 export const environment: Environment = {
   production: true,
   api: {
-    baseUrl: 'https://pr-26-prx-production.up.railway.app/prx',
+    baseUrl: 'https://pr-26-prx.onrender.com/prx',
     timeout: 10000,
   },
   storage: {
